c8bd27cf4dd384a797fbee7ecc75c55c56af04cf
befeleme <w_oczekiwaniu@interia.pl>
2020-12-21 19:29:13 +0100

    Solva day 21st puzzles

---
+++ 21/21a.py
@@ -0,0 +1,63 @@
+import fileinput
+import re
+
+
+def parse_input():
+    examples = [x.strip() for x in fileinput.input()]
+    foods_with_allergens = []
+    for example in examples:
+        matched = re.search(r'(?P<food>(\w+\s)+)\(contains (?P<alergens>(\w+)(, \w+)*)\)', example)
+        food = matched.group("food").split()
+        alergen = set(matched.group("alergens").split(", "))
+        foods_with_allergens.append((alergen, food))
+    return foods_with_allergens
+
+
+def get_count_of_all_ingredients(foods):
+    counter = 0
+    for _, food in foods:
+        counter += len(food)
+    return counter
+
+
+def get_all_allergens(foods):
+    allergens = set()
+    for alls, _ in foods:
+        for allergen in alls:
+            allergens.add(allergen)
+    allergens = list(allergens)
+    return allergens
+
+
+def check_food(allergens, foods):
+    for allergen in allergens:
+        if allergen in matches:
+            return
+        else:
+            matching = []
+            for all, food in foods:
+                if allergen in all:
+                    matching.append(set(food))
+            res = set.intersection(*matching)
+            if len(res) == 1:
+                res = res.pop()
+                matches[allergen] = res
+                erase_food_from_list(res, foods)
+                allergens.remove(allergen)
+                return check_food(allergens, foods)
+            else:
+                continue
+
+
+def erase_food_from_list(to_erase, foods):
+    for _, food in foods:
+        if to_erase in food:
+            food.remove(to_erase)
+
+
+foods_with_allergens = parse_input()
+allergens = get_all_allergens(foods_with_allergens)
+matches = {}
+check_food(allergens, foods_with_allergens)
+after = get_count_of_all_ingredients(foods_with_allergens)
+print(after)
