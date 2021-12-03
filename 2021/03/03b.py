05a72632a8d462c8a3f5f67778a767cd28f27c42
befeleme <w_oczekiwaniu@interia.pl>
2021-12-03 18:49:02 +0100

    Use recursive function instead of a for loop

--- 2021/03/03b.py
+++ 2021/03/03b.py
@@ -1,53 +1,35 @@
 import fileinput
 
+
+def find_rating(rating, leading_value, index=0):
+    bits = ("0", "1") if leading_value == "most_common_bits" else ("1", "0")
+
+    if len(rating) == 1:
+        return rating[0]
+
+    zeros, ones = count_zeros_and_ones(rating, index)
+    if zeros > ones:
+        rating = [x for x in rating if x[index] == bits[0]]
+    else:
+        rating = [x for x in rating if x[index] == bits[1]]
+
+    index += 1
+    return find_rating(rating, leading_value, index)
+
+
+def count_zeros_and_ones(rating, index):
+    zeros = ones = 0
+    for entry in rating:
+        if entry[index] == "0":
+            zeros += 1
+        else:
+            ones += 1
+    return zeros, ones
+
+
 report_numbers = [line.strip() for line in fileinput.input()]
 
-oxygen_generator_rating = ""
-co2_scrubber_rating = ""
-lookout_copy_oxy = list(report_numbers)
-lookout_copy_co2 = list(report_numbers)
-for i in range(len(report_numbers[0])):
-    zeros = 0
-    ones = 0
-
-    # oxygen
-    for entry in lookout_copy_oxy:
-
-        if entry[i] == "0":
-            zeros += 1
-        else:
-            ones += 1
-    else:
-        if zeros > ones:
-            lookout_copy_oxy = [x for x in lookout_copy_oxy if x[i] == "0"]
-
-        else:
-            lookout_copy_oxy = [x for x in lookout_copy_oxy if x[i] == "1"]
-
-        if len(lookout_copy_oxy) == 1:
-            oxygen_generator_rating = lookout_copy_oxy[0]
-
-    # co2
-    zeros = 0
-    ones = 0
-    for entry in lookout_copy_co2:
-
-        if entry[i] == "0":
-            zeros += 1
-        else:
-            ones += 1
-    else:
-        if zeros > ones:
-            lookout_copy_co2 = [x for x in lookout_copy_co2 if x[i] == "1"]
-
-        else:
-            lookout_copy_co2 = [x for x in lookout_copy_co2 if x[i] == "0"]
-
-        if len(lookout_copy_co2) == 1:
-            co2_scrubber_rating = lookout_copy_co2[0]
-
+oxygen_generator_rating = find_rating(list(report_numbers), "most_common_bits")
+co2_scrubber_rating = find_rating(list(report_numbers), "least_common_bits")
 
 print(int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2))
-
-
-
