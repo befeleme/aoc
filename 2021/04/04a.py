b47001324a9c2d7e45e929be97f6320dfcc896d3
befeleme <w_oczekiwaniu@interia.pl>
2021-12-04 17:42:30 +0100

    Refactor to OOP

--- 2021/04/04a.py
+++ 2021/04/04a.py
@@ -1,67 +1,92 @@
 import fileinput
 
-bingo = [line.strip().split() for line in fileinput.input()]
 
-draws = bingo.pop(0)
-draws = [int(d) for d in draws[0].split(",")]
+class BingoBoard:
+    def __init__(self, numbers):
+        self.board = self.create_board(numbers)
+        self.winner = False
 
-new_bingo = []
-board = []
-for board_line in bingo:
-    line = []
-    if board_line:
-        for no in board_line:
-            line.append((int(no), 0))
-        board.append(line)
+    @staticmethod
+    def create_board(numbers):
+        # create coordinates matrix [(4, 0), (4, 1)...]
+        coordinates_tuples = [(x, y) for x in reversed(range(5)) for y in range(5)]
+        return [Field(*c, no) for c, no in zip(coordinates_tuples, numbers)]
 
-    if len(board) == 5:
-        new_bingo.append(board)
-        board = []
+    def eval_win(self):
+        if self.winner:
+            return  # don't evaluate once game is won
+        # horizontal check
+        for i in range(0, 20, 5):
+            if len(list(filter(lambda x: x.hit, self.board[i:i+5]))) == 5:
+                self.winner = True
+                return True
 
-
-def is_win(bingo):
-    # horizontally:
-    for i, board in enumerate(bingo):
-        for bl in board:
-            counter = 0
-            for no, hit in bl:
-                if hit:
-                    counter += 1
-            if counter == 5:
-                return board
-    # vertically
-    for board in bingo:
+        # vertical check
+        # 0, 5, 10.. then 1, 6, 11..
         for i in range(5):
             counter = 0
-            for bl in board:
-                no, hit = bl[i]
-                if hit:
+            for j in range(i, 25, 5):
+                if self.board[j].hit:
                     counter += 1
-                if counter == 5:
-                    return board
+            if counter == 5:
+                self.winner = True
+                return True
 
-for draw in draws:
-    for k, board in enumerate(new_bingo):
-        for i, bl in enumerate(board):
-            for j, item in enumerate(bl):
-                no, hit = item
-                if draw == no:
-                    new_bingo[k][i][j] = (no, 1)
-    winner = is_win(new_bingo)
-    if winner:
-        print(winner)
-        break
+    def count_score(self, draw):
+        not_hit = list(filter(lambda x: not x.hit, self.board))
+        not_hit = [x.no for x in not_hit]
+        return sum(not_hit) * draw
 
-score = 0
-for bl in winner:
-    for no, hit in bl:
-        if not hit:
-            score += no
-
-score *= draw
-print(score)
+    def play_round(self, draw):
+        if self.winner:
+            return
+        for field in self.board:
+            if field.no == draw:
+                field.mark_hit()
+                break
 
 
+class Field:
+    def __init__(self, x, y, no):
+        self.x = x
+        self.y = y
+        self.no = no
+        self.hit = False
+
+    def __repr__(self):
+        is_hit = "hit" if self.hit else "not hit"
+        return f"({self.x}, {self.y}): {self.no} - {is_hit}"
+
+    def mark_hit(self):
+        self.hit = True
 
 
+def load_boards():
+    board = []
+    boards = []
+    for board_line in game_data:
+        if board_line:
+            for number in board_line:
+                board.append(int(number))
+        if len(board) == 25:
+            boards.append(BingoBoard(board))
+            board = []
+    return boards
 
+
+def play_game(draws, boards):
+    for draw in draws:
+        for game_board in boards:
+            game_board.play_round(draw)
+            if game_board.eval_win():
+                result = game_board.count_score(draw)
+                print("SCORE", result)
+                return
+
+
+if __name__ == "__main__":
+    game_data = [line.strip().split() for line in fileinput.input()]
+    draws = game_data.pop(0)
+    draws = [int(d) for d in draws[0].split(",")]
+    play_boards = load_boards()
+    play_game(draws, play_boards)
--- 2021/04/04b.py
+++ 2021/04/04b.py
@@ -1,86 +1,95 @@
 import fileinput
 
-bingo = [line.strip().split() for line in fileinput.input()]
 
-draws = bingo.pop(0)
-draws = [int(d) for d in draws[0].split(",")]
+class BingoBoard:
+    def __init__(self, numbers):
+        self.board = self.create_board(numbers)
+        self.winner = False
 
-new_bingo = []
-board = []
-for board_line in bingo:
-    line = []
-    if board_line:
-        for no in board_line:
-            line.append((int(no), 0))
-        board.append(line)
+    @staticmethod
+    def create_board(numbers):
+        # create coordinates matrix [(4, 0), (4, 1)...]
+        coordinates_tuples = [(x, y) for x in reversed(range(5)) for y in range(5)]
+        return [Field(*c, no) for c, no in zip(coordinates_tuples, numbers)]
 
-    if len(board) == 5:
-        new_bingo.append(board)
-        board = []
+    def eval_win(self):
+        if self.winner:
+            return  # don't evaluate once game is won
+        # horizontal check
+        for i in range(0, 20, 5):
+            if len(list(filter(lambda x: x.hit, self.board[i:i+5]))) == 5:
+                self.winner = True
+                return True
 
-
-def is_win(bingo):
-    winners = []
-    # horizontally:
-    for i, board in enumerate(bingo):
-        for bl in board:
-            counter = 0
-            for no, hit in bl:
-                if hit:
-                    counter += 1
-            if counter == 5:
-                winners.append(board)
-    # vertically
-    for board in bingo:
+        # vertical check
+        # 0, 5, 10.. then 1, 6, 11..
         for i in range(5):
             counter = 0
-            for bl in board:
-                no, hit = bl[i]
-                if hit:
+            for j in range(i, 25, 5):
+                if self.board[j].hit:
                     counter += 1
-                if counter == 5:
-                    if board not in winners:
-                        winners.append(board)
-    return winners
+            if counter == 5:
+                self.winner = True
+                return True
 
+    def count_score(self, draw):
+        not_hit = list(filter(lambda x: not x.hit, self.board))
+        not_hit = [x.no for x in not_hit]
+        return sum(not_hit) * draw
 
-winners_aside = []
-for draw in draws:
-    for k, board in enumerate(new_bingo):
-        for i, bl in enumerate(board):
-            for j, item in enumerate(bl):
-                no, hit = item
-                if draw == no:
-                    new_bingo[k][i][j] = (no, 1)
-    winners = is_win(new_bingo)
-    real_winner = ""
-    if winners:
-        for winner in winners:
-            if not winners_aside:
-                winners_aside.append(winner)
-            else:
-                for win in winners_aside:
-                    if win == winner:
-                        break
-                else:
-                    winners_aside.append(winner)
-            if len(winners_aside) == len(new_bingo):
-                real_winner = winner
+    def play_round(self, draw):
+        if self.winner:
+            return
+        for field in self.board:
+            if field.no == draw:
+                field.hit = True
                 break
-        if real_winner:
-            score = 0
-            for bl in winner:
-                for no, hit in bl:
-                    if not hit:
-                        score += no
-
-            score *= draw
-            print(score)
-            break
 
 
+class Field:
+    def __init__(self, x, y, no):
+        self.x = x
+        self.y = y
+        self.no = no
+        self.hit = False
+
+    def __repr__(self):
+        is_hit = "hit" if self.hit else "not hit"
+        return f"({self.x}, {self.y}): {self.no} - {is_hit}"
 
 
+def load_boards():
+    board = []
+    boards = []
+    for board_line in game_data:
+        if board_line:
+            for number in board_line:
+                board.append(int(number))
+        if len(board) == 25:
+            boards.append(BingoBoard(board))
+            board = []
+    return boards
 
 
+def is_last(boards):
+    if len(list(filter(lambda b: not b.winner, boards))) == 1:
+        return True
 
+
+def play_game(draws, boards):
+    for draw in draws:
+        for game_board in boards:
+            game_board.play_round(draw)
+            last = is_last(boards)
+            is_winner = game_board.eval_win()
+            if is_winner and last:
+                result = game_board.count_score(draw)
+                print("SCORE", result)
+
+
+if __name__ == "__main__":
+    game_data = [line.strip().split() for line in fileinput.input()]
+    draws = game_data.pop(0)
+    draws = [int(d) for d in draws[0].split(",")]
+    play_boards = load_boards()
+    play_game(draws, play_boards)
